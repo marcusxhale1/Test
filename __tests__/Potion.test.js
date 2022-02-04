d62dc4c11c6a182ae16f10e94333e47f9b1bd89d
Marcus Hale <marcusxhale1@gmail.com>
2022-02-04 01:06:04 -0800

    final commit for the night. Going to shower, brush teeth, and sleeeeep

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,12 +1,18 @@
-const { expect } = require('@jest/globals');
-const { string } = require('yargs');
 const Potion = require('../Potion');
 
 
-test('creates a random potion object', () => {
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+  
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+  });
+  
+  test('creates a random potion object', () => {
     const potion = new Potion();
   
     expect(potion.name).toEqual(expect.any(String));
     expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
   });
+  
